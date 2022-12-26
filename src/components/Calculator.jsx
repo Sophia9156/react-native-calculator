4f8367bc264ac559492672bb2a8565448458a2a4
Sophia <91585668+Sophia9156@users.noreply.github.com>
2022-12-26 10:47:54 +0900

    중간 계산 결과 보여주기 추가

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -62,78 +62,15 @@ const InputContainer = styled.View`
 `;
 
 export default function Calculator() {
-  const [input, setInput] = useState(0);
-  const [expression, setExpression] = useState([]);
-  const [currentOperator, setCurrentOperator] = useState(null);
-  const [result, setResult] = useState(null);
-  const [tempInput, setTempInput] = useState(null);
-  const [tempOperator, setTempOperator] = useState(null);
-  const [isClickedOperator, setClickedOperator] = useState(false);
-  const [isClickedEqual, setClickedEqual] = useState(false);
-
-  const hasInput = !!input;
-
-  const onPressNum = (num) => {
-    if (currentOperator && isClickedOperator) {
-      setResult(input);
-      setInput(num);
-      setClickedOperator(false);
-    } else {
-      const newInput = Number(`${input}${num}`);
-      setInput(newInput);
-    }
-  };
-
-  const onPressOperator = (operator) => {
-    if (operator === "=") {
-      let finalResult = result;
-      const finalInput = isClickedEqual ? tempInput : input;
-      const finalOperator = isClickedEqual ? tempOperator : currentOperator;
-      switch (currentOperator) {
-        case "+":
-          finalResult = result + finalInput;
-          break;
-        case "-":
-          finalResult = result - finalInput;
-          break;
-        case "*":
-          finalResult = result * finalInput;
-          break;
-        case "/":
-          finalResult = result / finalInput;
-          break;
-        default:
-          break;
-      }
-      setExpression([...expression, `${input}`]);
-      setResult(finalResult);
-      setInput(finalResult);
-      setTempInput(finalInput);
-      setCurrentOperator(null);
-      setTempOperator(finalOperator);
-      setClickedEqual(true);
-    } else {
-      setExpression([...expression, `${input} ${operator} `]);
-      setCurrentOperator(operator);
-      setClickedOperator(true);
-      setClickedEqual(false);
-    }
-  };
-
-  const onPressReset = () => {
-    if (hasInput) {
-      // C
-      setInput(0);
-    } else {
-      // AC
-      setInput(0);
-      setCurrentOperator(null);
-      setResult(null);
-      setTempInput(null);
-      setTempOperator(null);
-      setExpression([]);
-    }
-  };
+  const {
+    input,
+    expression,
+    hasInput,
+    currentOperator,
+    onPressNum,
+    onPressOperator,
+    onPressReset,
+  } = useCalculator();
 
   return (
     <View style={{ flex: 1, width: "100%", justifyContent: "center" }}>
--- src/hooks/useCalculator.js
+++ src/hooks/useCalculator.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 export const useCalculator = () => {
   const [input, setInput] = useState(0);
+  const [expression, setExpression] = useState([]);
   const [currentOperator, setCurrentOperator] = useState(null);
   const [result, setResult] = useState(null);
   const [tempInput, setTempInput] = useState(null);
   const [tempOperator, setTempOperator] = useState(null);
   const [isClickedOperator, setClickedOperator] = useState(false);
+  const [isClickedOperatorMoreThanOnce, setClickedOperatorMoreThanOnce] = useState(false);
   const [isClickedEqual, setClickedEqual] = useState(false);
 
   const hasInput = !!input;
@@ -24,34 +26,60 @@ export const useCalculator = () => {
 
   const onPressOperator = (operator) => {
     if (operator === "=") {
-      let finalResult = result;
-      const finalInput = isClickedEqual ? tempInput : input;
-      const finalOperator = isClickedEqual ? tempOperator : currentOperator;
-      switch (currentOperator) {
-        case "+":
-          finalResult = result + finalInput;
-          break;
-        case "-":
-          finalResult = result - finalInput;
-          break;
-        case "*":
-          finalResult = result * finalInput;
-          break;
-        case "/":
-          finalResult = result / finalInput;
-          break;
-        default:
-          break;
+      if (result) {
+        let finalResult = result;
+        const finalInput = isClickedEqual ? tempInput : input;
+        const finalOperator = isClickedEqual ? tempOperator : currentOperator;
+        switch (currentOperator) {
+          case "+":
+            finalResult = result + finalInput;
+            break;
+          case "-":
+            finalResult = result - finalInput;
+            break;
+          case "*":
+            finalResult = result * finalInput;
+            break;
+          case "/":
+            finalResult = result / finalInput;
+            break;
+          default:
+            break;
+        }
+        setResult(finalResult);
+        setInput(finalResult);
+        setTempInput(finalInput);
+        setCurrentOperator(null);
+        setExpression([...expression, `${input}`])
+        setTempOperator(finalOperator);
+        setClickedEqual(true);
       }
-      setResult(finalResult);
-      setInput(finalResult);
-      setTempInput(finalInput);
-      setCurrentOperator(null);
-      setTempOperator(finalOperator);
-      setClickedEqual(true);
     } else {
+      if (currentOperator && isClickedOperatorMoreThanOnce) {
+        let finalResult = result;
+        switch (currentOperator) {
+          case "+":
+            finalResult = result + input;
+            break;
+          case "-":
+            finalResult = result - input;
+            break;
+          case "*":
+            finalResult = result * input;
+            break;
+          case "/":
+            finalResult = result / input;
+            break;
+          default:
+            break;
+        }
+        setResult(finalResult);
+        setInput(finalResult);
+      }
       setCurrentOperator(operator);
+      setExpression([...expression, `${input} ${operator} `]);
       setClickedOperator(true);
+      setClickedOperatorMoreThanOnce(true);
       setClickedEqual(false);
     }
   };
@@ -67,16 +95,15 @@ export const useCalculator = () => {
       setResult(null);
       setTempInput(null);
       setTempOperator(null);
+      setExpression([]);
     }
   };
 
   return {
     input,
+    expression,
     hasInput,
     currentOperator,
-    result,
-    tempInput,
-    tempOperator,
     onPressNum,
     onPressOperator,
     onPressReset,
